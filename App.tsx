cba219f9c54b9d213c0b68afce7c3f628c401503
Junior Dering <juniordering@hotmail.com>
2023-12-02 14:43:13 -0400

    Feat: fonts and loading component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
-import { SignIn } from './src/screens/SignIn';
+import { ThemeProvider } from 'styled-components'
+import { SignIn } from './src/screens/SignIn'
+import theme from './src/theme'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { Loading } from './src/components/Loading'
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
+  if (fontsLoaded) {
+    return <Loading />
+  }
+
   return (
-    <SignIn />
-  );
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
+  )
 }
