10c4e2564864ab98e3003597d39d960f0c050143
Junior Dering <juniordering@hotmail.com>
2023-12-03 11:06:21 -0400

    feat: login with google

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,18 +1,46 @@
-import { Container, Slogan, Title } from './styles';
+import { Container, Slogan, Title } from './styles'
 
 import backgroundImg from '../../assets/background.png'
-import { Button } from '../../components/Button';
+import { Button } from '../../components/Button'
+import { ANDROID_CLIENT_ID, WEB_CLIENT_ID } from '@env'
+import { useEffect, useState } from 'react'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
+import { Alert } from 'react-native'
+
+GoogleSignin.configure({
+  scopes: ['email', 'profile'],
+  webClientId: WEB_CLIENT_ID,
+})
 
 export function SignIn() {
+
+  const [isAuthenticating, setIsAuthenticaiting] = useState(false)
+
+  const handleGoogleSignIn = async () => {
+    setIsAuthenticaiting(true)
+    try {
+      await GoogleSignin.hasPlayServices()
+      const response = await GoogleSignin.signIn()
+
+      if (response.idToken) {
+        console.log('ESSE É O TOKEN: => ', response)
+      }
+    } catch (error) {
+      Alert.alert('Não foi possível conectar-se a sua conta Google')
+    } finally {
+      setIsAuthenticaiting(false)
+    }
+  }
+
   return (
     <Container source={backgroundImg}>
-      <Title>
-        Ignite Fleet
-      </Title>
-      <Slogan>
-        Gestão de uso de veículos
-      </Slogan>
-      <Button title='Entrar com Google' />
+      <Title>Ignite Fleet</Title>
+      <Slogan>Gestão de uso de veículos</Slogan>
+      <Button
+        title="Entrar com Google"
+        onPress={handleGoogleSignIn}
+        isLoading={isAuthenticating}
+      />
     </Container>
-  );
+  )
 }
